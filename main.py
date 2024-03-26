6fe97ab2bab40a9c014fdc9fd00fb322c122cf6c
David Arteaga <david.arteaga@revinate.com>
2024-03-26 17:16:20 -0300

    yolo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def duck():
     print("hi")
-    return f'suck my duck 🦆'
+    return f'suck my duckerfile 🦆'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000' ,debug=True)
