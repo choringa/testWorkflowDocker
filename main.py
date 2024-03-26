186de00b43452b7e562606f31140e874561cb17d
David Arteaga <david.arteaga@revinate.com>
2024-03-26 12:38:54 -0300

    yolo

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ def hello():
     content = request.json
     return f'Hello, World! {content}'
 
+@app.route('/')
+def hello():
+    return f'suck my duck 🦆'
+
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', port='8080' ,debug=True)
