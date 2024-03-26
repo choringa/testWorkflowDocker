6dcd82f981aad203aa9f02cd94a66eaefff4ed3d
David Arteaga <david.arteaga@revinate.com>
2024-03-26 12:52:41 -0300

    yolo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/webhooked')
+@app.route('/webhooked', methods =['POST'])
 def hello():
     content = request.json
     return f'Hello, World! {content}'
