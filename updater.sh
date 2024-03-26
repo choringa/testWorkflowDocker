5eb3aed7350b50a7ad8b8e82dd1c407c0a147190
David Arteaga <david.arteaga@revinate.com>
2024-03-26 17:12:04 -0300

    yolo

--- updater.sh
+++ updater.sh
@@ -1,3 +1,7 @@
-kill -9 $(ps -aux | grep 'main.py' | awk '{split($0, a," "); print a[2]}' | head -1)
+process=$(ps -aux | grep 'main.py' | awk '{split($0, a," "); print a[2]}' | head -1)
+echo "1. Killing process id: " $process
+kill -9 $process
+echo "2. Pulling new code"
 git -C /home/ubuntu/testWorkflowDocker pull
+echo "3. Restart service"
 python3 /home/ubuntu/testWorkflowDocker/main.py & 
