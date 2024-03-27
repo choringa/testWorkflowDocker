b3c2d0c6ca75d037fb311059902a30ebea138414
David Arteaga <david.arteaga@revinate.com>
2024-03-27 12:25:31 -0300

    yolo

--- updater.sh
+++ updater.sh
@@ -1,7 +1,8 @@
-process=$(ps -aux | grep 'main.py' | awk '{split($0, a," "); print a[2]}' | head -1)
-echo "1. Killing process id: " $process
-kill -9 $process
+#process=$(ps -aux | grep 'main.py' | awk '{split($0, a," "); print a[2]}' | head -1)
+#echo "1. Killing process id: " $process
+#kill -9 $process
 echo "2. Pulling new code"
 git -C /home/ubuntu/testWorkflowDocker pull
 echo "3. Restart service"
-python3 /home/ubuntu/testWorkflowDocker/main.py & 
+#sudo systemctl restart vms
+#python3 /home/ubuntu/testWorkflowDocker/main.py & 
--- updater_server.py
+++ updater_server.py
@@ -1,7 +1,26 @@
 from flask import Flask, request
 import subprocess
+import hashlib
+import hmac
 
 app = Flask(__name__)
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise Exception(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise Exception(status_code=403, detail="Request signatures didn't match!")
 
 def update():
     print ("Starting update")
