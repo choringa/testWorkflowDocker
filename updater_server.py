34dba6bdca440ff7b685d94e9b52b7943085127f
David Arteaga <david.arteaga@revinate.com>
2024-03-27 14:55:03 -0300

    yolo

--- updater_server.py
+++ updater_server.py
@@ -30,12 +30,12 @@ def hello():
     request_dict = request.json
     print(f"type: {type(request_dict)}, request_dict: {request_dict}")
     if(request.headers.get("X-Hub-Signature-256")):
-        author_dict = request_dict.get('author')
-        print(f"2: type: {type(author_dict)}, author_dict: {author_dict}")
+        commits_list = request_dict.get('commits')
+        print(f"2: type: {type(commits_list)}, author_dict: {commits_list}")
         print (f"Changes on VMS repository \
-                Last change made by: {author_dict.get('name')}({author_dict.get('email')}) \
-                added: {len(request_dict.get('added'))}, removed: {len(request_dict.get('removed'))}, modified: {len(request_dict.get('modified'))} \
-                commit reference: {request_dict.get('after')}")
+                    Last change made by: {commits_list[0].get('name')}({commits_list[0].get('email')}) \
+                    added: {len(commits_list[0].get('added'))}, removed: {len(commits_list[0].get('removed'))}, modified: {len(commits_list[0].get('modified'))} \
+                    commit reference: {request_dict.get('after')}")
         print(f"hub ignature: {request.headers.get('X-Hub-Signature-256')}")
         secret = os.getenv("GITHUB_WEBHOOK_SECRET")
         if(secret):
